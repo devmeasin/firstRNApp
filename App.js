472eb6a5f7a507cb90ac8333d5dc0b5ec583049a
Easin Webpro <easinislam299@gmail.com>
2022-12-23 23:42:27 +0600

    :lipstick: [app.js] add navigation system stack navigation

--- App.js
+++ App.js
@@ -12,22 +12,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screen/Home';
 import Login from './src/screen/Login';
+import BottomTabs from './src/components/BottomTabs';
 
 
 const App = () => {
 
   const Stack = createNativeStackNavigator();
- 
+
   return (
 
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="BottomTabs" component={BottomTabs} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
 
-    
+
   );
 };
 
